ab029ad5ad4f2987ed88d056c9d570dffdb365b9
AliEsad48 <alicamci2020@gmail.com>
2021-09-06 13:50:56 +0300

    V1

---
+++ src/projectvendingmachine/Runner.java
@@ -0,0 +1,15 @@
+package projectvendingmachine;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Products product = new Products();
+
+        Methods methods = new Methods();
+        methods.select(product);
+
+
+    }
+
+}
